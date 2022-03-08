b886f0cd99ebeeffece753af96962eb5f8f43c71
Oguzhan YAGCI <oyagci@pm.me>
2022-03-08 13:10:31 +0100

    Add abstractions over shaders and window handling

--- main.go
+++ main.go
@@ -5,10 +5,10 @@ import (
 	"fmt"
 	"log"
 	"runtime"
-	"strings"
 	"unsafe"
 
 	"github.com/oyagci/renderer-go/program"
+	"github.com/oyagci/renderer-go/renderer/window"
 
 	"github.com/go-gl/gl/v4.6-core/gl"
 	"github.com/go-gl/glfw/v3.3/glfw"
@@ -102,12 +102,17 @@ func main() {
 	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
 	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
 
-	window, err := glfw.CreateWindow(800, 600, "Testing", nil, nil)
+	windowProps := window.WindowProps{
+		Width:  800,
+		Height: 600,
+		Title:  "OpenGL Window",
+	}
+	window, err := window.CreateWindow(windowProps)
 
 	if err != nil {
 		panic(err)
 	}
-	window.MakeContextCurrent()
+	window.GetInternal().MakeContextCurrent()
 
 	if err = gl.Init(); err != nil {
 		panic(err)
@@ -178,74 +183,15 @@ func main() {
 
 	vertexArray.AddBufferObject(&bufferObject)
 
-	for !window.ShouldClose() {
+	for !window.GetInternal().ShouldClose() {
 		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 
-		program.UseGLProgram(*shaderProgram)
+		shaderProgram.Bind()
 
 		gl.BindVertexArray(uint32(vertexArray.GetId()))
 		gl.DrawElementsWithOffset(gl.TRIANGLES, int32(len(triangleMesh.GetData().Indices)), gl.UNSIGNED_INT, uintptr(triangleMesh.GetIndicesOffset()))
 
-		window.SwapBuffers()
+		window.GetInternal().SwapBuffers()
 		glfw.PollEvents()
 	}
-}
-
-func newProgram(vertexShaderSource string, fragmentShaderSource string) (uint32, error) {
-
-	vertexShader, err := compileShader(vertexShaderSource, gl.VERTEX_SHADER)
-	if err != nil {
-		panic(err)
-	}
-
-	fragmentShader, err := compileShader(fragmentShaderSource, gl.FRAGMENT_SHADER)
-	if err != nil {
-		panic(err)
-	}
-
-	program := gl.CreateProgram()
-
-	gl.AttachShader(program, vertexShader)
-	gl.AttachShader(program, fragmentShader)
-	gl.LinkProgram(program)
-
-	var status int32
-	gl.GetProgramiv(program, gl.LINK_STATUS, &status)
-	if status == gl.FALSE {
-		var logLength int32
-		gl.GetProgramiv(program, gl.INFO_LOG_LENGTH, &logLength)
-
-		log := strings.Repeat("\x00", int(logLength+1))
-		gl.GetProgramInfoLog(program, logLength, nil, gl.Str(log))
-
-		return 0, fmt.Errorf("failed to link program: %v", log)
-	}
-
-	gl.DeleteShader(vertexShader)
-	gl.DeleteShader(fragmentShader)
-
-	return program, nil
-}
-
-func compileShader(source string, shaderType uint32) (uint32, error) {
-	shader := gl.CreateShader(shaderType)
-
-	csources, free := gl.Strs(source)
-	gl.ShaderSource(shader, 1, csources, nil)
-	free()
-	gl.CompileShader(shader)
-
-	var status int32
-	gl.GetShaderiv(shader, gl.COMPILE_STATUS, &status)
-	if status == gl.FALSE {
-		var logLength int32
-		gl.GetShaderiv(shader, gl.INFO_LOG_LENGTH, &logLength)
-
-		log := strings.Repeat("\x00", int(logLength+1))
-		gl.GetShaderInfoLog(shader, logLength, nil, gl.Str(log))
-
-		return 0, fmt.Errorf("failed to compile %v: %v", source, log)
-	}
-
-	return shader, nil
 }
---
+++ renderer/window/window.go
@@ -0,0 +1,29 @@
+package window
+
+import "github.com/go-gl/glfw/v3.3/glfw"
+
+type WindowProps struct {
+	Width  int
+	Height int
+	Title  string
+}
+
+type RendererWindow struct {
+	window *glfw.Window
+}
+
+func CreateWindow(props WindowProps) (*RendererWindow, error) {
+	window, err := glfw.CreateWindow(props.Width, props.Height, props.Title, nil, nil)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &RendererWindow{
+		window: window,
+	}, nil
+}
+
+func (w *RendererWindow) GetInternal() *glfw.Window {
+	return w.window
+}
